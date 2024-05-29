1e39dd259d8014d55b515d33029976a0a5f3d847
notmyproblemm <147689995+notmyproblemm@users.noreply.github.com>
2024-05-29 22:50:09 +0300

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace LAB8;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Client(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+}
